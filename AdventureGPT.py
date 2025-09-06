6ba95a7c74c233166329821e022c3908b152c362
Malind Kowalski <malindkowalski@gmail.com>
2025-09-06 12:36:42 -0600

    testing out if for activity type and state options

--- AdventureGPT.py
+++ AdventureGPT.py
@@ -15,6 +15,24 @@ Nesting is KEY!
 # hiking = 1, biking = 2, waterday = 3
 # if activitytype == 1 then go into hiking options and etc for 2 and 3
 
-enter = input("Enter 1:Start or 2:Stop ")
+# activity type
+# state option
+# hiking: easy, hard
+# biking: easy, hard
+# water day: free, fee
 
+
+hiking = 1
+biking = 2
+waterDay = 3
+
+enter = input("Enter 1 :Start or 2 :Stop ")
 while enter != "2":
+    activityType = input("Enter 1 :Hiking, 2: Biking, 3 :Water Day :")
+    if activityType == "1":
+        stateOption = input("Enter State for activity: CO, NM, UT, AZ: ")
+        print("you chose state: ", stateOption)
+    break
+        
+    # print("yay we're here!")
+    # break
