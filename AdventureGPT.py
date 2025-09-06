378d8f0ff11492804ff56f79cf2348d212b81832
Malind Kowalski <malindkowalski@gmail.com>
2025-09-06 13:19:29 -0600

    thinking of logic for difficulty level/state options

--- AdventureGPT.py
+++ AdventureGPT.py
@@ -20,6 +20,8 @@ Nesting is KEY!
 # hiking: easy, hard
 # biking: easy, hard
 # water day: free, fee
+# once state is entered, i'm asking user about difficluty level but how do i keep stateOption?
+# i don't want it to print an actiivity with a difficulty level for NM when CO was entered
 
 
 hiking = 1
@@ -31,7 +33,20 @@ while enter != "2":
     activityType = input("Enter 1 :Hiking, 2: Biking, 3 :Water Day :")
     if activityType == "1":
         stateOption = input("Enter State for activity: CO, NM, UT, AZ: ")
-        print("you chose state: ", stateOption)
+        print("Great you chose", stateOption, "\n")
+        level = input("Enter 1 :Easy, 2 :Hard :")
+        
+        # if 1easy then show easy hiking place
+        if level == "1":
+            print("Okay!  You can do an easy hike in", stateOption, "at Falls Creek Meadows in Durango CO. \n")
+        # else 2 hard then show hard hiking place
+        else:
+            print("Okay! You can do a hard hike in", stateOption, "up Engineer Mountain in the San Juan Mountains. \n")
+    # elif 2:
+        # stateOption
+    # else 3:
+        # stateOption
+        # print("you chose state: ", stateOption)
     break
         
     # print("yay we're here!")
