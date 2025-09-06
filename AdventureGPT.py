c4e24af1630a40e05560500280f904656ba188c7
Malind Kowalski <malindkowalski@gmail.com>
2025-09-06 12:23:50 -0600

    thinking of activity type and logic of selection

--- AdventureGPT.py
+++ AdventureGPT.py
@@ -10,3 +10,11 @@ Nesting is KEY!
 *50 lines minimum*
 '''
 
+# activity type: hiking, biking, water day
+# once entered, ask for which type of activity(with numbers?)
+# hiking = 1, biking = 2, waterday = 3
+# if activitytype == 1 then go into hiking options and etc for 2 and 3
+
+enter = input("Enter 1:Start or 2:Stop ")
+
+while enter != "2":
