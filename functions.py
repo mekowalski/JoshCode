fb69f91d6de3af41e8dd4f89ad0e9f28bc3bd854
Malind Kowalski <malindkowalski@gmail.com>
2025-09-27 12:59:30 -0600

    another funtion written

--- functions.py
+++ functions.py
@@ -1,11 +1,26 @@
-def personInfo(name, age, nationality):
-    print("Welcome:", name "\n")
-    print("You are:", age "\n")
-    print("You are:", nationality "\n")
+# def personInfo(name, age, nationality):
+#     print("Welcome:", name, "\n")
+#     print("You are:", age, "\n")
+#     print("You are:", nationality, "\n")
     
-number = int(input("Amount: "))
-for i in range(number):
-    name = input("Enter first name: ").capitalize()
-    age = input("Enter your age: ")
-    nationality = input("Enter your nationality: ").capitalize()
-    personInfo(name, age, nationality)
+# for i in range(2):
+#     name = input("Enter first name: ").capitalize()
+#     age = input("Enter your age: ")
+#     nationality = input("Enter your nationality: ").capitalize()
+#     personInfo(name, age, nationality)
+
+def goodDeal(cost):
+    if cost >= 60 and cost < 120:
+        response = "This is a great deal!"
+    elif cost >= 120:
+        response = "This is 'tlai-a'!"
+    else:
+        response = "This is cheap! BUY NOW"
+    return response
+
+store = input("Enter your store name: ")
+cost = int(input("Item price: "))
+res = goodDeal(cost)
+print(store, "-", res)
+if res == "This is a great deal!":
+    print("Buy before it's too late!")
