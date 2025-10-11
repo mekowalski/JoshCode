ada07dc0f5d346587cea1f08385998f43ff4b7af
Malind Kowalski <malindkowalski@gmail.com>
2025-10-11 13:01:51 -0600

    added another functions

--- functions2.py
+++ functions2.py
@@ -1,6 +1,6 @@
 # def grade(percentage):
 #     if percentage > 0 and percentage <= 50:
-#         print("IMPROVE YPUR GRADE!!!")
+#         print("IMPROVE YOUR GRADE!!!")
 #     elif percentage > 50 and percentage <= 70:
 #         print("You're barely passing the class")
 #     elif percentage > 70 and percentage <= 90:
@@ -11,17 +11,35 @@
 # percentage = int(input("Please enter your grade percentage: "))
 # grade(percentage)
 
-def bankBalance(balance):
-    if balance >= 500:
-        return True
+# def bankBalance(balance):
+#     if balance >= 500:
+#         return True
+#     else:
+#         return False
+
+# name = input("Please enter your name: ").capitalize()
+# balance = float(input("Please enter your current balance: "))
+
+# res = bankBalance(balance)
+# if res == True:
+#     print("Your balance is above $500. You have enough funds \n")
+# else:
+#     print("Your balance is below $500. Bank balance is low \n")
+    
+def mortgage(deposit):
+    if deposit >= 50000:
+        print("You're Approved!!! \n")
+    elif deposit >= 20000 and deposit < 50000:
+        print("You need an approval process. \n")
     else:
-        return False
-
-name = input("Please enter your name: ").capitalize()
-balance = float(input("Please enter your current balance: "))
-
-res = bankBalance(balance)
-if res == True:
-    print("Your balance is above $500. You have enough funds \n")
-else:
-    print("Your balance is below $500. Bank balance is low \n")
+        print("Not approved for a mortgage. \n")
+        
+name = input("Enter name: ")
+deposit = int(input("Enter deposit: "))
+totalAmount = 0
+while deposit != 0:
+    totalAmount += deposit
+    mortgage(deposit)
+    deposit = int(input("Deposit amount: "))
+print("Total amount requested: ", totalAmount)
+    
