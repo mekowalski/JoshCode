c465b3b573a8d944327aa1674370ae5194c792ed
Malind Kowalski <malindkowalski@gmail.com>
2025-10-11 14:06:02 -0600

    complete bmi functions

--- functions3.py
+++ functions3.py
@@ -1,5 +1,16 @@
-def bmiCalculator():
-    weight = int(input("Enter weight(lbs): \n")
-    height = int(input("Enter height(in): \n")
-    bmi = weight / (height * height)
+def bmiCalculator(weight, height):
+    bmi = (weight / (height * height)) * 703
     return bmi
+
+def bmiResult(weight, height):
+    calculate = bmiCalculator(weight, height)
+    if calculate <= 18.5:
+        print("Underweight")
+    elif calculate > 18.5 and calculate <= 25:
+        print("Normal")
+    else:
+        print("Overweight")
+    
+weight = int(input("Enter weight(lbs): "))
+height = int(input("Enter height(in): "))
+bmiResult(weight, height)
